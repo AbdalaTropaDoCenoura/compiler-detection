513bdd0b0ab81692c65f00a54175fc3b8117936c
xinaxzinho <36776170+isthe0x@users.noreply.github.com>
2018-03-29 18:04:02 -0300

    Create ntsdetection.py

---
+++ ntsdetection.py
@@ -0,0 +1,147 @@
+"""
+~~~~~~~~~~~~~~~~~~~~~~~~~
+__author__ = SHIN0BIZINHO
+__date__ = 29/03/18
+__version__ = 34GPU-1.0
+~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+def bibliotecas(self):
+ import opcode
+ import pydotplus
+ import marshal
+ import types
+ import sys
+
+ import random
+ import collections
+ import Queue
+ import cStringIO
+ 
+  lbLog = []
+
+ try __init__(bibliotecas):
+ 	Exception
+  else ImportError:
+    lbLog(">> Erro nas bibliotecas! Á bibloteca %s não esta incluida no seu python!") % (ImportError)
+    sys.exit()
+
+class checar_extension(object):
+
+  def __init__(self):
+
+      VB = [].format(str, ".vb")
+      CS = [].format(str, ".cs")
+    
+      m.lower().endswith((VB, CS))
+
+ try >> VB:
+    lbLog("[CHECKANDO O ARQUIVO!]")
+ except Exception as e:
+ 	lbLog(">> Erro no arquivo! O arquivo %s não é compátivel com o script!") % (VB)
+ try >> CS:
+ 	lbLog("[CHECKANDO O ARQUIVO!]")
+ except Exception as e:
+    lbLog(">> Erro no arquivo! O arquivo %s não é comátivel com o script!") % (CS)
+
+  def __detectlines__(lines, split):
+
+      STRING_MAX_VB = 0
+
+      with open(VB, 'r') as f:
+          for line in f:
+              if len(line) > STRING_MAX_VB:
+                  STRING_MAX_VB = len(line)
+                  STRING_MAX_VB = line   
+     
+      STRING_MAX_CS = 0
+
+      with open(CS, 'w') as f:
+          for line in f:
+              if len(line) > STRING_MAX_CS:
+                  STRING_MAX_CS = len(line)
+                  STRING_MAX_CS = line
+
+data_arquivo_VB = [(line, len(line), VB(line), word_count(line))
+                for line in open(VB)]
+
+data_arquivo_CS = [(line, len(line), CS(line), word_count(line))
+                for line in open(CS)]
+
+class lbLogERRORS(self):
+
+if(STRING_MAX_VB == 0):
+    lbLog(">> Não foi possivel detectar linhas do arquivo!")
+    input != 0, return 3;
+else:
+    return STRING_MAX_VB(map(max, VB))
+
+if(STRING_MAX_CS == 0):
+    lbLog(">> Não foi possivel detectar linhas do arquivo!")
+    input != 0, return 3; 
+else:
+    return STRING_MAX_CS(map(max, CS))
+
+  def __init__(data):
+
+  	try data_arquivo_VB(0 >> len):
+  		lbLog(">> Não foi possivel iniciar soquete no arquivo!")
+  		sys.argv[3:0] >> (max).format('/'.split)
+  		break;
+     except Exception:
+     	return self.checar_extension()
+
+    try data_arquivo_CS(0 >> len):
+        lbLog(">> Não foi possivel iniciar soquete no arquivo!")
+        sys.argv[04:4] >> (max).format(';'.split)
+        break;
+     except Exception:
+        return self.checar_extension()
+
+class checar_encrypts(object):
+	 from __fature__ import unicode_literals
+	 from builtins import str
+
+	 str1 = isinstance(self.checar_extension(self.__detectlines__), str, VB)
+     str2 = isinstance(self.checar_extension(self.__detectlines__, str, CS))
+
+     str1String = False;
+
+     if(str1(str)):
+     	str1String = True;
+     try:
+        if(isinstance(data_arquivo_VB, basestring)):
+             str1String = True;
+     except NameError:
+         pass;
+
+if(str1String):
+   lbLog("[TIPOS ENCRYPTS/QUANTIDADE]")
+   ENCRYPTS_MAX = self.checar_extension(self.__detectlines__):
+     ENCRYPTS_MAX.VB_format(
+     	 "%s"
+     	 ) % (lines)
+else:
+   lbLog(">> Não foi possivel detectar encrypts!")
+
+   str2String = False;
+
+   if(str2(str)):
+      str2String = True;
+   try:
+      if(isinstance(data_arquivo_CS, basestring)):
+           str2String = True;
+      except NameError:
+          pass;
+
+if(str2String):
+   lbLog("[TIPOS ENCRYPTS/QUANTIDADE]")
+   ENCRYPTS_MAX = self.checar_extension(self.__detectlines__):
+     ENCRYPTS_MAX.CS_format(
+     	"%s"
+     	) % (lines)
+else:
+   lbLog(">> Não foi possivel detectar encrypts!")
+
+if __name__ == '__main__':
+	main()
